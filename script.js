498bea128a8d68f8969ebd26f857e9b6d8db110d
d3kay <fahim.anwar1997@gmail.com>
2022-12-01 21:16:35 +0100

    added functions for calculating operations

---
+++ script.js
@@ -0,0 +1,60 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.allClear()
+    }
+    allClear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    } 
+    delete() {
+
+    }
+    appendNumber(number) {
+        this.currentOperand = number
+    }
+    chooseOperation(operation) {
+
+    }
+    compute() {
+
+    }
+    updateDisplay () {
+        this.currentOperandTextElement.innerText = this.currentOperand
+    }
+}
+
+const previousOperandTextElement = document.querySelector (
+    "[data-previous-operand]"
+)
+
+const currentOperandTextElement = document.querySelector (
+    "[data-current-operand]"
+)
+
+const numberButtons = document.querySelectorAll(
+    "[data-number]"
+)
+const operationsButtons = document.querySelectorAll(
+    "[data-operation]"
+)
+const equalButton = document.querySelector(
+    "[data-equal]"
+)
+const deleteButton = document.querySelector(
+    "[data-delete]"
+)
+const allClearButton = document.querySelector(
+    "[data-all-clear]"
+)
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
